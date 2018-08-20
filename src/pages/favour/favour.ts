e1437b2cd86c87ea022bc54ff2b637bebe083793
Juan Medina <soyjuanmedina@gmail.com>
2018-08-21 00:35:45 +0200

    Deleting my favours

--- src/pages/favour/favour.ts
+++ src/pages/favour/favour.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { DatabaseProvider } from "../../providers/database/database";
+
 /**
  * Generated class for the FavourPage page.
  *
@@ -16,15 +18,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class FavourPage {
   favour; 
   itsMine: boolean = false;
+  editing: boolean = false;
 
   constructor(public navCtrl: NavController,
-    public navParams: NavParams) {
+    public navParams: NavParams,
+    private _DB: DatabaseProvider) {
       console.log(navParams);
       this.favour = this.navParams.get('favour');
       if(localStorage.userId == this.favour.askedUserId || localStorage.email == this.favour.askedMail){
         this.itsMine = true;
       }
+  }
 
+  editFavour(){
+    console.log(this.editing);
+    this.editing = true;
+    console.log(this.editing);
+  }
+
+  deleteFavour(){
+    this._DB.deleteDocument("favours", this.favour.id)
+    .then(data => {
+      console.log('eliminado');
+      this.navCtrl.pop();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    
   }
 
   ionViewDidLoad() {
